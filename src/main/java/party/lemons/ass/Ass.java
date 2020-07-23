8c263b3ebfe87753319e7f21ff8590c6b818569e
Sam <lemonverse@gmail.com>
2020-07-23 18:32:19 +1200

    item group

--- src/main/java/party/lemons/ass/Ass.java
+++ src/main/java/party/lemons/ass/Ass.java
@@ -1,13 +1,19 @@
 package party.lemons.ass;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import party.lemons.ass.init.AssBlocks;
 import party.lemons.ass.init.AssScreens;
 import party.lemons.ass.util.registry.RegistryLoader;
 
 public class Ass implements ModInitializer
 {
 	public static final String MODID = "ass";
+
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(ID(MODID), ()->new ItemStack(AssBlocks.SCORCHED_FURNACE));
 
 	@Override
 	public void onInitialize()
--- src/main/java/party/lemons/ass/init/AssItems.java
+++ src/main/java/party/lemons/ass/init/AssItems.java
@@ -1,11 +1,12 @@
 package party.lemons.ass.init;
 
 import net.minecraft.item.Item;
+import party.lemons.ass.Ass;
 
 public class AssItems
 {
 	public static Item.Settings settings()
 	{
-		return new Item.Settings();
+		return new Item.Settings().group(Ass.ITEM_GROUP);
 	}
 }
