79b746a11264aff64eb24f1d45a8347bcb61662c
Sam <lemonverse@gmail.com>
2020-07-23 18:55:46 +1200

    hardness

--- src/main/java/party/lemons/ass/init/AssBlocks.java
+++ src/main/java/party/lemons/ass/init/AssBlocks.java
@@ -15,22 +15,22 @@ import party.lemons.ass.util.registry.AutoReg;
 @AutoReg(registry = "block", type = Block.class)
 public class AssBlocks
 {
-	public static final FueledFurnaceBlock SCORCHED_FURNACE = new FueledFurnaceBlock(RecipeType.SMELTING, Util.lazy(()->AssBlockEntities.FUELED_SMELTING_FURNACE), settings(Material.METAL));
-	public static final FueledFurnaceBlock SCORCHED_SMOKER = new FueledFurnaceBlock(RecipeType.SMOKING, Util.lazy(()->AssBlockEntities.FUELED_SMOKER), settings(Material.METAL));
-	public static final FueledFurnaceBlock SCORCHED_BLAST_FURNACE = new FueledFurnaceBlock(RecipeType.BLASTING, Util.lazy(()->AssBlockEntities.FUELED_BLAST_FURNACE), settings(Material.METAL));
-	public static final SpoutBlock SPOUT = new SpoutBlock(settings(Material.METAL).nonOpaque());
-	public static final BlockBreakerBlock BLOCK_BREAKER = new BlockBreakerBlock(settings(Material.METAL));
-	public static final BlockPlacerBlock BLOCK_PLACER = new BlockPlacerBlock(settings(Material.METAL));
-	public static final GatedHopperBlock GATED_HOPPER = new GatedHopperBlock(settings(Material.METAL));
-	public static final ExperienceTorchBlock EXPERIENCE_TORCH = new ExperienceTorchBlock(FabricBlockSettings.of(Material.METAL).nonOpaque().breakInstantly());
+	public static final FueledFurnaceBlock SCORCHED_FURNACE = new FueledFurnaceBlock(RecipeType.SMELTING, Util.lazy(()->AssBlockEntities.FUELED_SMELTING_FURNACE), settings(Material.STONE));
+	public static final FueledFurnaceBlock SCORCHED_SMOKER = new FueledFurnaceBlock(RecipeType.SMOKING, Util.lazy(()->AssBlockEntities.FUELED_SMOKER), settings(Material.STONE));
+	public static final FueledFurnaceBlock SCORCHED_BLAST_FURNACE = new FueledFurnaceBlock(RecipeType.BLASTING, Util.lazy(()->AssBlockEntities.FUELED_BLAST_FURNACE), settings(Material.STONE));
+	public static final SpoutBlock SPOUT = new SpoutBlock(settings(Material.METAL, 3.5F).nonOpaque());
+	public static final BlockBreakerBlock BLOCK_BREAKER = new BlockBreakerBlock(settings(Material.STONE));
+	public static final BlockPlacerBlock BLOCK_PLACER = new BlockPlacerBlock(settings(Material.STONE));
+	public static final GatedHopperBlock GATED_HOPPER = new GatedHopperBlock(settings(Material.METAL, 5.0F));
+	public static final ExperienceTorchBlock EXPERIENCE_TORCH = new ExperienceTorchBlock(settings(Material.METAL, 1.5F).noCollision());
 	public static final CreativeGeneratorBlock CREATIVE_GENERATOR = new CreativeGeneratorBlock(FabricBlockSettings.of(Material.METAL).dropsNothing().strength(-1));
-	public static final FurnaceGeneratorBlock FURNACE_GENERATOR = new FurnaceGeneratorBlock(settings(Material.METAL));
-	public static final CableBlock CABLE = new CableBlock(settings(Material.METAL));
-	public static final PoweredFurnaceBlock POWERED_SCORCHED_FURNACE = new PoweredFurnaceBlock(RecipeType.SMELTING, Util.lazy(()->AssBlockEntities.POWERED_SMELTING_FURNACE), settings(Material.METAL));
-	public static final PoweredFurnaceBlock POWERED_SCORCHED_SMOKER = new PoweredFurnaceBlock(RecipeType.SMOKING, Util.lazy(()->AssBlockEntities.POWERED_SMOKER), settings(Material.METAL));
-	public static final PoweredFurnaceBlock POWERED_SCORCHED_BLAST_FURNACE = new PoweredFurnaceBlock(RecipeType.BLASTING, Util.lazy(()->AssBlockEntities.POWERED_BLAST_FURNACE), settings(Material.METAL));
-	public static final EnderMagnetBlock ENDER_MAGNET = new EnderMagnetBlock(settings(Material.METAL));
-	public static final BubbleGeneratorBlock BUBBLE_GENERATOR = new BubbleGeneratorBlock(settings(Material.METAL));
+	public static final FurnaceGeneratorBlock FURNACE_GENERATOR = new FurnaceGeneratorBlock(settings(Material.STONE));
+	public static final CableBlock CABLE = new CableBlock(settings(Material.PISTON, 1.0F));
+	public static final PoweredFurnaceBlock POWERED_SCORCHED_FURNACE = new PoweredFurnaceBlock(RecipeType.SMELTING, Util.lazy(()->AssBlockEntities.POWERED_SMELTING_FURNACE), settings(Material.STONE));
+	public static final PoweredFurnaceBlock POWERED_SCORCHED_SMOKER = new PoweredFurnaceBlock(RecipeType.SMOKING, Util.lazy(()->AssBlockEntities.POWERED_SMOKER), settings(Material.STONE));
+	public static final PoweredFurnaceBlock POWERED_SCORCHED_BLAST_FURNACE = new PoweredFurnaceBlock(RecipeType.BLASTING, Util.lazy(()->AssBlockEntities.POWERED_BLAST_FURNACE), settings(Material.STONE));
+	public static final EnderMagnetBlock ENDER_MAGNET = new EnderMagnetBlock(settings(Material.STONE));
+	public static final BubbleGeneratorBlock BUBBLE_GENERATOR = new BubbleGeneratorBlock(settings(Material.STONE));
 
 	public static FabricBlockSettings settings(Material material, float hardness)
 	{
@@ -39,6 +39,6 @@ public class AssBlocks
 
 	public static FabricBlockSettings settings(Material material)
 	{
-		return settings(material, 1.5F);
+		return settings(material, 3.5F);
 	}
 }
--- src/main/java/party/lemons/ass/init/AssItems.java
+++ src/main/java/party/lemons/ass/init/AssItems.java
@@ -1,8 +1,11 @@
 package party.lemons.ass.init;
 
+import net.minecraft.block.TorchBlock;
 import net.minecraft.item.Item;
 import party.lemons.ass.Ass;
+import party.lemons.ass.util.registry.AutoReg;
 
+@AutoReg(type = Item.class, registry = "item")
 public class AssItems
 {
 	public static Item.Settings settings()
